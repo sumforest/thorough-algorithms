834a27731a2ef0659666b6a469882e50a06531b0
Sen <12345678>
2020-01-19 02:00:35 +0800

    Huffman

---
+++ src/test/java/BinaryTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+/**
+ * @Auther: Sen
+ * @Date: 2020/1/19 00:13
+ * @Description:
+ */
+public class BinaryTest {
+
+    @Test
+    public void testBinary(){
+        System.out.println((byte)Integer.parseInt("10101000",2));
+    }
+}
